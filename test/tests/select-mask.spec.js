0f9c22d87a06087cb6a5f0ff18d90943521f3e80
Justin Perry <just.perry@gmail.com>
2014-04-22 19:22:37 +0100

    Add RequireJS call
    Dummy test now passes

--- test/tests/select-mask.spec.js
+++ test/tests/select-mask.spec.js
@@ -1,6 +1,9 @@
 describe("Select mask", function() {
-  before(function() {
+  before(function(done) {
     var html = window.__html__['test/fixtures/select-mask.html'];
+    require(['jquery'], function($) {
+      done();
+    });
   });
   it('should fail', function() {
     expect(true).to.be.false;
