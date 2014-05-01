d937e1ddc62900d1274526aee5ae9f77a5af96f1
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-01 17:06:48 +0100

    Add more documentation

--- src/js/select-mask.js
+++ src/js/select-mask.js
@@ -1,88 +1,164 @@
-/*
-  - Set label element's inner text
-  - find element
-  - get inner text of select
-  - set inner text of label
-- Update title of select
-  - get inner text of select
-  - set title
-- Inject label
-  - create detached element [DONE]
-  - set default text [DONE]
-  - attach element to DOM [DONE]
-- Set focus state of element
-  - [TBD]
-*/
-
-  function SelectMask(options) {
-    if(typeof options !== 'object') return;
-
-    this.selectors = {
-      select: options.selectors.select || '.js-select-mask'
-    };
-    this.elementType = options.elementType || 'span';
-    this.$selects = null;
-    this.eventFn = $.proxy(this._update,this);
-    this._init();
-  }
-
-  // 'Private' methods
-  SelectMask.prototype._bindEvents = function() {
-    $(document).on('change', this.selectors._update, this.eventFn);
+/**
+ * @class SelectMask
+ * @constructor
+ * @param {Object} options                    Configuration parameters
+ * @param {Object} [options.selectors]        Selectors to use
+ * @param {String} [options.selectors.select] Selectors to use
+ */
+function SelectMask(options) {
+  this.selectors = {
+    select: options.selectors.select || '.js-select-mask'
   };
+  this.elementType = options.elementType || 'span';
+  this.$selects = null;
+  this.eventFn = this._updateLabel.bind(this);
+  this._init();
+}
 
-  SelectMask.prototype._unbindEvents = function() {
-    $(document).off('change', this.selectors._update, this.eventFn);
+/**
+ * Unbinds onchange events from matching `select` elements in this.selects
+ * @method _unbindEvents
+ * @private
+ * @return {Object} this
+ */
+SelectMask.prototype._init = function() {
+  this.$selects = $(this.selectors.select);
+
+  return this;
+};
+
+/**
+ * Binds onchange events to `select` elements in this.selects
+ * @method _bindEvents
+ * @private
+ */
+SelectMask.prototype._bindEvents = function() {
+  $(document).on('change', this.selectors.select, this.eventFn);
+};
+
+/**
+ * Unbinds onchange events from matching `select` elements in this.selects
+ * @method _unbindEvents
+ * @private
+ */
+SelectMask.prototype._unbindEvents = function() {
+  $(document).off('change', this.selectors.select, this.eventFn);
+};
+
+/**
+ * Updates the label mask
+ * @method _updateLabel
+ * @private
+ * @return {Object} this 
+ */
+SelectMask.prototype._updateLabel = function(e) {
+  this._setLabelMask(e.target.previousSibling, this._getSelectedIndex(e.target).innerText);
+  console.log('var1');
+  return this;
+};
+
+/**
+ * Creates mask DOM element and prepends to `select` element
+ * @method _createLabelMask
+ * @param {Object} select `select` element
+ * @private
+ * @return {Object} this
+ */
+SelectMask.prototype._createLabelMask = function(select) {
+  var mask = document.createElement(this.elementType);
+  this._setLabelMask(mask, this._getSelectedIndex(select).innerText);
+  mask.className += 'select-mask';
+  select.parentNode.insertBefore(mask, select);
+
+  return this;
+};
+
+/**
+ * Creates mask DOM element and prepends to `select` element
+ * @method _setLabelMask
+ * @param {Object} elem Mask element
+ * @param {String} text Text to set on the label mask
+ * @private
+ * @return {Object} this
+ */
+SelectMask.prototype._setLabelMask = function(elem, text) {
+  console.log('_setLabelMask', elem, text);
+  elem.innerText = text;
+  return this;
+};
+
+/**
+ * Loops over the selects array and calling the _createLabelMask method on each
+ * @method _processSelects
+ * @private
+ * @return {Object} this
+ */
+SelectMask.prototype._processSelects = function() {
+  var that = this;
+  $.each(this.$selects, function() {
+    that._createLabelMask($(this)[0]);
+  });
+  return this;
+};
+
+/**
+ * Gets the currently selected index from the passed `select` element
+ * @method _getSelecteIndex
+ * @param {Object} select `select` element
+ * @private
+ * @return {Number} The selected index
+ */
+SelectMask.prototype._getSelectedIndex = function(select) {
+  var options = select.options;
+  return options[options.selectedIndex];
+};
+
+/**
+ * Adds DOM events
+ * @method start
+ * @public
+ * @return {Object} this
+ */
+SelectMask.prototype.start = function() {
+  this._bindEvents();
+  this._processSelects();
+
+  return this;
+};
+
+/**
+ * Destroys the select masks and unbinds any event listeners
+ * @method destroy
+ * @public
+ * @return {Object} this
+ */
+SelectMask.prototype.destroy = function() {
+  this._unbindEvents();
+};
+
+/**
+ * ES5 Shims
+ */
+if(typeof Function.prototype.bind === 'undefined') {
+  return function( event ){
+    return fn.call( context, ( event || window.event ) );
   };
+}
 
-  SelectMask.prototype._init = function() {
-    this.$selects = $(this.selectors.select);
-
-    return this;
+/**
+ * Events
+ */
+if( window.addEventListener ){
+  var eventHelper = {
+    addEventMethodName : 'addEventListener',
+    removeEventMethodName : 'removeEventListener',
+    prefix : ''
   };
-
-  SelectMask.prototype._update = function(e) {
-    this._setLabelMask(e.target.previousSibling, this._getSelectedIndex(e.target).innerText);
-
-    return this;
+}
+else{
+  var eventHelper = {
+    addEventMethodName : 'attachEvent',
+    removeEventMethodName : 'detachEvent',
+    prefix : 'on'
   };
-
-  SelectMask.prototype._getSelectedIndex = function(select) {
-    var options = select.options;
-    return options[options.selectedIndex];
-  };
-
-  SelectMask.prototype._createLabelMask = function(select) {
-    var mask = document.createElement(this.elementType);
-    this._setLabelMask(mask, this._getSelectedIndex(select).innerText);
-    mask.className += 'select-mask';
-    select.parentNode.insertBefore(mask, select);
-
-    return this;
-  };
-
-  SelectMask.prototype._setLabelMask = function(elem, text) {
-    elem.innerText = text;
-    return this;
-  };
-
-  SelectMask.prototype._processSelects = function() {
-    var that = this;
-    $.each(this.$selects, function() {
-      that._createLabelMask($(this)[0]);
-    });
-    return this;
-  };
-
-  // Public methods
-  SelectMask.prototype.start = function() {
-    this._bindEvents();
-    this._processSelects();
-
-    return this;
-  };
-
-  SelectMask.prototype.stop = function() {
-    this._unbindEvents();
-    // Remove label mask
-  };
+}
