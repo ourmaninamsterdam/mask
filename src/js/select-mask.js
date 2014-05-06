be52c09d78fef418e468daddf3cae7cea326c017
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-06 16:43:27 +0100

    Start Vanilla JS rebuild

--- src/js/select-mask.js
+++ src/js/select-mask.js
@@ -4,6 +4,7 @@
  * @param {Object} options                    Configuration parameters
  * @param {Object} [options.selectors]        Selectors to use
  * @param {String} [options.selectors.select] Selectors to use
+ * @return {Object} this
  */
 function SelectMask(options) {
   this.selectors = {
@@ -11,18 +12,18 @@ function SelectMask(options) {
   };
   this.elementType = options.elementType || 'span';
   this.$selects = null;
-  this.eventFn = this._updateLabel.bind(this);
+  this.eventFn = this._update.bind(this);
   this._init();
 }
 
 /**
- * Unbinds onchange events from matching `select` elements in this.selects
+ * Caches `select` elements
  * @method _unbindEvents
  * @private
  * @return {Object} this
  */
 SelectMask.prototype._init = function() {
-  this.$selects = $(this.selectors.select);
+  this.$selects = this.selectors.select;
 
   return this;
 };
@@ -31,42 +32,60 @@ SelectMask.prototype._init = function() {
  * Binds onchange events to `select` elements in this.selects
  * @method _bindEvents
  * @private
+ * @return {Object} this
  */
 SelectMask.prototype._bindEvents = function() {
-  $(document).on('change', this.selectors.select, this.eventFn);
+  var that = this;
+  document.addEventListener('change', function(e){
+    that._delegateEvent();
+  });
+    // .on('change', , this.eventFn)
+    // .on('focus', this.selectors.select, this._setMaskFocusState);
+
+  return this;
+};
+
+SelectMask.prototype._delegateEvent = function(elem, selector) {
+  var regEx = new RegExp(selector,'i');
+  if(e.target.className.test(selector)){
+    console.log('match');
+  }
 };
 
 /**
  * Unbinds onchange events from matching `select` elements in this.selects
  * @method _unbindEvents
  * @private
+ * @return {Object} this
  */
 SelectMask.prototype._unbindEvents = function() {
   $(document).off('change', this.selectors.select, this.eventFn);
+
+  return this;
 };
 
 /**
- * Updates the label mask
- * @method _updateLabel
+ * Updates the mask
+ * @method _update
  * @private
  * @return {Object} this 
  */
-SelectMask.prototype._updateLabel = function(e) {
-  this._setLabelMask(e.target.previousSibling, this._getSelectedIndex(e.target).innerText);
-  console.log('var1');
+SelectMask.prototype._update = function(e) {
+  this._setMaskText(e.target.previousSibling, this._getSelectedIndex(e.target).innerText);
+
   return this;
 };
 
 /**
  * Creates mask DOM element and prepends to `select` element
- * @method _createLabelMask
+ * @method _createMask
  * @param {Object} select `select` element
  * @private
  * @return {Object} this
  */
-SelectMask.prototype._createLabelMask = function(select) {
+SelectMask.prototype._createMask = function(select) {
   var mask = document.createElement(this.elementType);
-  this._setLabelMask(mask, this._getSelectedIndex(select).innerText);
+  this._setMaskText(mask, this._getSelectedIndex(select).innerText);
   mask.className += 'select-mask';
   select.parentNode.insertBefore(mask, select);
 
@@ -75,29 +94,33 @@ SelectMask.prototype._createLabelMask = function(select) {
 
 /**
  * Creates mask DOM element and prepends to `select` element
- * @method _setLabelMask
+ * @method _setMaskText
  * @param {Object} elem Mask element
- * @param {String} text Text to set on the label mask
+ * @param {String} text Text to set on the mask
  * @private
  * @return {Object} this
  */
-SelectMask.prototype._setLabelMask = function(elem, text) {
-  console.log('_setLabelMask', elem, text);
+SelectMask.prototype._setMaskText = function(elem, text) {
+  console.log('_setMaskText', elem, text);
   elem.innerText = text;
+
   return this;
 };
 
 /**
- * Loops over the selects array and calling the _createLabelMask method on each
+ * Loops over the selects array and calling the _createMask method on each
  * @method _processSelects
  * @private
  * @return {Object} this
  */
 SelectMask.prototype._processSelects = function() {
-  var that = this;
-  $.each(this.$selects, function() {
-    that._createLabelMask($(this)[0]);
-  });
+  var that = this,
+    i = this.$selects.length;
+    
+  while(i--) {
+    that._createMask(this.$selects[i]);
+  }
+
   return this;
 };
 
@@ -110,7 +133,21 @@ SelectMask.prototype._processSelects = function() {
  */
 SelectMask.prototype._getSelectedIndex = function(select) {
   var options = select.options;
+
   return options[options.selectedIndex];
+};
+
+/**
+ * Adds a focus class to the mask
+ * @method _setMaskTextFocusState
+ * @param {Object} elem The mask element
+ * @private
+ * @return {Number} this
+ */
+SelectMask.prototype._setMaskFocusState = function(elem) {
+  elem.className += 'is-active';
+
+  return this;
 };
 
 /**
@@ -134,31 +171,33 @@ SelectMask.prototype.start = function() {
  */
 SelectMask.prototype.destroy = function() {
   this._unbindEvents();
+
+  return this;
 };
 
 /**
  * ES5 Shims
  */
-if(typeof Function.prototype.bind === 'undefined') {
-  return function( event ){
-    return fn.call( context, ( event || window.event ) );
-  };
-}
+// if(typeof Function.prototype.bind === 'undefined') {
+//   return function( event ){
+//     return fn.call( context, ( event || window.event ) );
+//   };
+// }
 
-/**
- * Events
- */
-if( window.addEventListener ){
-  var eventHelper = {
-    addEventMethodName : 'addEventListener',
-    removeEventMethodName : 'removeEventListener',
-    prefix : ''
-  };
-}
-else{
-  var eventHelper = {
-    addEventMethodName : 'attachEvent',
-    removeEventMethodName : 'detachEvent',
-    prefix : 'on'
-  };
-}
+// /**
+//  * Events
+//  */
+// if( window.addEventListener ){
+//   var eventHelper = {
+//     addEventMethodName : 'addEventListener',
+//     removeEventMethodName : 'removeEventListener',
+//     prefix : ''
+//   };
+// }
+// else{
+//   var eventHelper = {
+//     addEventMethodName : 'attachEvent',
+//     removeEventMethodName : 'detachEvent',
+//     prefix : 'on'
+//   };
+// }
