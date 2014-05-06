1718f104da5f534df3fb718e22a3ce53d2662991
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-06 16:43:17 +0100

    Move karma config to /test folder

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,11 +1,11 @@
 module.exports = function(config) {
   config.set({
-    basePath: '',
+    basePath: '../',
     frameworks: ['requirejs','mocha', 'chai', 'sinon'],
     files: [
       'test/fixtures/*.html',
       'test/test-main.js',
-      {pattern: 'js/**/*.js', included: false},
+      {pattern: 'src/js/**/*.js', included: false},
       {pattern: 'test/tests/*.spec.js', included: false},
       {pattern: 'bower_components/**/*.js', included: false}
     ],
