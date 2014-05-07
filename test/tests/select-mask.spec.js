d3578de232169ce900208fe0f967dcab0c55ee72
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-07 17:00:05 +0100

    Require select-mask module in spec

--- test/tests/select-mask.spec.js
+++ test/tests/select-mask.spec.js
@@ -1,11 +1,24 @@
-describe("Select mask", function() {
+describe('Select Mask', function() {
+  var selectMask, elemFixture, fixtureHTML;
+
   before(function(done) {
-    var html = window.__html__['test/fixtures/select-mask.html'];
-    require(['jquery'], function($) {
+    fixtureHTML = window.__html__['test/fixtures/select-mask.html'];
+    elemFixture = document.querySelector('select');
+    console.log('ee', document);
+
+    require(['select-mask'], function(SelectMask) {
+      console.log('SSSSSSS:',SelectMask);
+      selectMask = new SelectMask();
       done();
     });
   });
-  it('should fail', function() {
-    expect(true).to.be.false;
+
+  describe('Selecting an option', function() {
+    before(function() {
+
+    });
+    it('the mask text should match selected option', function() {
+      expect(true).to.be.false;
+    });
   });
 });
